306aa06fec87b0bd3a7eed1c64110d6d22b18362
saadfrhan <saadfarhan347@gmail.com>
2024-10-14 09:39:34 +0500

    update

---
+++ 2-reverse-string.js
@@ -0,0 +1,8 @@
+let word = "hello";
+let reversed = "";
+
+for (let i = 0; i <= word.length - 1; i++) {
+  reversed += word[word.length - 1 - i];
+}
+
+console.log(reversed);
---
+++ README.md
@@ -0,0 +1,4 @@
+# javascript-problems
+
+- [FizzBuzz](./1-fizz-buzz.js)
+- [Reverse a String](./2-reverse-string.js)
